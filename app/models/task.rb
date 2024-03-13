292c6bcd74913a27fd99f891f0c73868a6aaad4b
emjeic <marliejohnc.official@gmail.com>
2024-03-13 20:41:25 +0800

    Add validation for task creation date_cannot_be_in_the_past

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,5 +6,15 @@ class Task < ApplicationRecord
   validates :task, presence: true
   validates :date, presence: true, length: { minimum: 10 }
 
+  validate :date_cannot_be_in_the_past
+
   scope :incomplete_for_today, -> { where(completed: false, date: Date.today) }
+end
+
+private
+
+def date_cannot_be_in_the_past
+  if date.present? && date < Date.today
+    errors.add(:date, "can't be in the past")
+  end
 end
