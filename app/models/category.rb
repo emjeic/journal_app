57581ac765212ce527ae1eb9b3ffebaad4cc00ca
emjeic <marliejohnc.official@gmail.com>
2024-03-23 00:42:36 +0800

    Applied css for my project

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ApplicationRecord
   has_many :tasks, dependent: :destroy
   
   validates :title, presence: true
-  validates :description, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 20 }
 
   # method to check for existing complete and incomplete tasks
   def has_complete_and_incomplete_tasks?
